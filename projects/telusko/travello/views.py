54a7a5eead51c4d12ffa84aef4e1c180efe199ca
Thonteshila <thonte.shila>
2023-01-05 20:04:32 +0530

    Database connectivity done

--- projects/telusko/travello/views.py
+++ projects/telusko/travello/views.py
@@ -5,27 +5,5 @@ from .models import Destination
 
 
 def index(request):
-
-    dest1 = Destination()
-    dest1.name = 'Mumbai'
-    dest1.desc = 'The city never sleep'
-    dest1.img = 'destination_1.jpg'
-    dest1.price = 800
-    dest1.offer=False
-
-    dest2 = Destination()
-    dest2.name = 'Hyderabad'
-    dest2.desc = 'Hyderabad biryanii is awaysome'
-    dest2.img = 'destination_2.jpg'
-    dest2.price = 400
-    dest2.offer=True
-
-    dest3 = Destination()
-    dest3.name = 'Pune'
-    dest3.desc = 'Nice city'
-    dest3.img = 'destination_3.jpg'
-    dest3.price = 1100
-    dest3.offer=False
-
-    dests =[dest1,dest2,dest3]
+    dests=Destination.objects.all()
     return render(request, 'index.html', {'dests': dests})
